9de467691143034a035ca471d06ea4a36e870dcf
Ekaterina Ilin <eilin@aip.de>
2023-04-18 08:51:47 +0200

    added the light curves

---
+++ src/scripts/FIGURE_lightcurves.py
@@ -0,0 +1,52 @@
+"""
+Python 3.8 - UTF-8
+
+X-ray Loops
+Ekaterina Ilin, 2023
+MIT License
+
+---
+
+This script reads in stacked PN and MOS, and optical monitoring light curves 
+from XMM-Newton, and plots them together.
+"""
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import paths
+
+
+if __name__ == "__main__":
+
+    # read in optical data
+    om = pd.read_csv("../data/timeseries.csv")
+    om.time = om.time / 3600. / 24.
+    om.rate = om.rate / np.nanmedian(om.rate)
+
+    # read in X-ray data
+    xray = pd.read_csv("../data/stacked_xray_lightcurve.csv")
+    xray.time = xray.time / 3600. / 24.
+
+    # make the figure
+
+    fix, ax = plt.subplots(2, 1, sharex=True, figsize=(8, 6),
+                        gridspec_kw={'height_ratios': [2, 3],
+                                            'wspace':0, 'hspace':0})
+    ax[0].scatter(om.time, om.rate, label="OM", s=2)
+    ax[1].plot(xray.time, xray.normalized_flux + 1., label="PN + MOS1 + MOS2")
+    ax[0].set_ylim(0,3)
+
+    for a in ax:
+        a.axhline(1, c="grey")
+        a.set_xlim(om.time.min(), xray.time.max())
+        a.legend(loc=1, frameon=True)
+
+
+    ax[0].set_ylabel("normalized flux")
+    ax[1].set_ylabel("normalized flux")
+    ax[1].set_xlabel("time [days]")
+
+    plt.tight_layout()
+    plt.savefig(paths.figures / "lightcurves.png", dpi=300)
