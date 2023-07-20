42f9533bb818a2db24e69e7e82f8e6b1028a66b5
Ekaterina Ilin <eilin@aip.de>
2023-07-20 15:19:10 +0200

    bump up energy conversion by one oom

--- src/scripts/TABLE_tess_flares.py
+++ src/scripts/TABLE_tess_flares.py
@@ -22,11 +22,11 @@ if __name__ == "__main__":
     sel = df[["tstart", "ampl_rec", 'ed_rec', 'ed_rec_err', 'Sector']].sort_values("tstart")
 
     # convert to 10^30 erg
-    sel["ed_rec"] = sel["ed_rec"] / 1e30
-    sel["ed_rec_err"] = sel["ed_rec_err"] / 1e30
+    sel["ed_rec"] = sel["ed_rec"] / 1e31
+    sel["ed_rec_err"] = sel["ed_rec_err"] / 1e31
 
     # format
-    sel[r"$E_{\rm bol}$ [$10^{30}$ erg]"] = (sel["ed_rec"].round(2).astype(str) + 
+    sel[r"$E_{\rm bol}$ [$10^{31}$ erg]"] = (sel["ed_rec"].round(2).astype(str) + 
                                             " [" + 
                                             sel["ed_rec_err"].round(2).astype(str) + 
                                             "]")
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     sel = sel.rename(columns={"tstart": r"$t_{s}$ [BJD - 2457000]", "ampl_rec": r"$a$"})
 
     # select columns in the correct order
-    sel = sel[[r"$t_{s}$ [BJD - 2457000]", r"$a$", r"$E_{\rm bol}$ [$10^{30}$ erg]", "Sector"]]
+    sel = sel[[r"$t_{s}$ [BJD - 2457000]", r"$a$", r"$E_{\rm bol}$ [$10^{31}$ erg]", "Sector"]]
 
     # convert to LaTeX
     string = sel.to_latex(index=False, escape=False)
