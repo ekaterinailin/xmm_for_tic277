5dcc81749c3bf9e7edb64e0c22a94c78028110b1
Ekaterina Ilin <eilin@aip.de>
2023-08-08 11:23:37 +0200

    added the values script and fixed a typo

--- src/scripts/paper_figures_and_tables.sh
+++ src/scripts/paper_figures_and_tables.sh
@@ -1,7 +1,9 @@
 python FIGURE_data_resid.py
 python FIGURE_lightcurves.py
 python FIGURE_tess_ffd.py
-python FIGURE_tess_lc.py
+python FIGURE_tess_lcs.py
 
 python TABLE_specfits.py
 python TABLE_tess_flares.py
+
+python VALUES.py
